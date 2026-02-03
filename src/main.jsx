75c587de072fb1a68b745ead3d4b9dc5dc1f7107
Gad Anaclet Irumva <irumvagadanaclet@gmail.com>
2026-02-03 16:55:45 +0200

    feat:startup codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createElement } from "react"
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById("root"))
+const reactElement=createElement("h1", null, "Hello from createElement!")
+console.log(reactElement)
+root.render(
+	reactElement
 )
