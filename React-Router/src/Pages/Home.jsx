d0d5fd90cd0da96d13f3d3918a3ce517a81fa57d
Gad Anaclet Irumva <irumvagadanaclet@gmail.com>
2026-03-31 19:22:07 +0200

    Feat: props and css styling

--- React-Router/src/Pages/Home.jsx
+++ React-Router/src/Pages/Home.jsx
@@ -1,4 +1,15 @@
+import data from './data.js'
+import ExpenseEntryItemList from '../component/ExpenseEntryItemList'
+import ExpenseEntryItem from '../component/ExpenseEntryItem.jsx'
+
 function Home() {
-   return <h3>Home</h3>
+   return (
+      <>
+         <h3>Home</h3>
+         <ExpenseEntryItemList items={data} key={20}/>
+         <hr />
+         <ExpenseEntryItem />
+      </>
+      )
 }
 export default Home
