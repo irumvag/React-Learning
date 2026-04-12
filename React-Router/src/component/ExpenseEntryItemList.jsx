189e1f07c42b32c295e57dbfef6b08c020c88260
Gad Anaclet Irumva <irumvagadanaclet@gmail.com>
2026-04-12 22:38:17 +0200

    feat:react event api

--- React-Router/src/component/ExpenseEntryItemList.jsx
+++ React-Router/src/component/ExpenseEntryItemList.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 class ExpenseEntryItemList extends React.Component {
    constructor(props) {
       super(props);
+      this.state={
+
+      }
    }
    componentDidMount() {
       console.log("ExpenseEntryItemList :: Initialize :: componentDidMount :: Component mounted");
@@ -25,9 +28,18 @@ class ExpenseEntryItemList extends React.Component {
    componentWillUnmount() {
       console.log("ExpenseEntryItemList :: Remove :: componentWillUnmount :: Component unmounted");
    }
+   handleMouseEnter(e){
+      e.target.parentNode.classList.add("highlight");
+   }
+   handleMouseLeave(e){
+      e.target.parentNode.classList.remove('highlight');
+   }
+   handleMouseOver(e){
+      console.log("The mouse is at (" + e.clientX + ", " + e.clientY + ")");
+   }
    render() {
       const lists = this.props.items.map( (item) => 
-         <tr key={item.id}>
+         <tr key={item.id} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
             <td>{item.name}</td>
             <td>{item.amount}</td>
             <td>{new Date(item.spendDate).toDateString()}</td>
@@ -37,7 +49,7 @@ class ExpenseEntryItemList extends React.Component {
       return (
          <>
          <h1>This is the expense Item details</h1>
-         <table>
+         <table onMouseOver={this.handleMouseOver}>
             <thead>
                <tr>
                   <th>Item</th>
